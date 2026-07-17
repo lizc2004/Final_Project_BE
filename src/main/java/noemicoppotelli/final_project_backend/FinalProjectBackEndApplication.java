15b8bb68b64dea39693e76206e4eefab4dd65ef5
Noemi Coppotelli <liz.c2004@gmail.com>
2026-07-17 10:00:41 +0200

    setup project

---
+++ src/main/java/noemicoppotelli/final_project_backend/FinalProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package noemicoppotelli.final_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/noemicoppotelli/final_project_backend/FinalProjectBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package noemicoppotelli.final_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
